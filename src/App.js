8ebb95a5eb26cbebb9295ea1cfb71dce0dc791aa
hzhang20902 <85701939+hzhang20902@users.noreply.github.com>
2022-10-05 22:20:52 -0400

    banner design no logic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-
+import { Download, Features, SectionWrapper } from "./components";
+import assets from './assets'
 
 const App = () => {
   return (
-    <div>
-     <h1 className="text-3xl font-bold underline">React Native NFT Showcase</h1>
-    </div>
+    <>
+      <SectionWrapper 
+        title="Your own store of nifty NFTs. 
+        Start Selling & Growing"
+        
+        description="Buy, store, collect NFTs- exchange and earn crypto.
+        Join 25+ MILLION people using ProNef Marketplace."
+
+        showBtn
+        mockupImg={assets.homeHero}
+        banner="banner"
+      />
+    </>
   );
 }
 

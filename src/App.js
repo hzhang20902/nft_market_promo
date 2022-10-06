2be3591e300f847bbd4da4f8685b939d3df60b9f
hzhang20902 <85701939+hzhang20902@users.noreply.github.com>
2022-10-05 23:17:07 -0400

    finished styling added gh button link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Download, Features, SectionWrapper } from "./components";
 import assets from './assets'
+import styles from './styles/Global'
 
 const App = () => {
   return (
@@ -24,6 +25,35 @@ const App = () => {
         mockupImg={assets.homeCards}
         reverse
       />
+
+      <Features />
+
+      <SectionWrapper 
+        title="Deployment"
+
+        description="ProNef is built using Expo, which runs natively on any devices' OS. 
+        You can get your mobile app into people's hands."
+
+        mockupImg={assets.feature}
+        reverse
+      />
+
+      <SectionWrapper 
+        title="Creative way to showcase the store"
+
+        description="The ProNef Marketplace contains two screens that render multiple asset pages. 
+        The home page lists all available NFTs with the current bid price. The individual NFT asset pages showcase the details and bid history of that particular work."
+
+        mockupImg={assets.mockup}
+        banner='banner02'
+      />
+
+      <Download />
+      <div className="px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04">
+        <p className={`
+        ${styles.pText}
+        ${styles.whiteText}`}>Made with love, sweat, and fears by {" "}<span className="bold">Henry Zhang</span></p>
+      </div>
     </>
   );
 }
--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import styles from '../styles/Global'
+import assets from '../assets'
 
 const Download = () => {
   return (
-    <div>Download</div>
+    <div className={`
+    ${styles.section}
+    ${styles.bgWhite}`}>
+      <div className={`
+      ${styles.subSection}
+      flex-col
+      text-center`}>
+        <div>
+          <h1 className={`
+          ${styles.h1Text}
+          ${styles.blackText}`}>Download the Source Code</h1>
+          <p className={`
+          ${styles.pText}
+          ${styles.blackText}`}>Get the full source code on GitHub</p>
+        </div>
+        <button className={styles.btnPrimary}
+        onClick={() => window.open('https://github.com/hzhang20902/reactn_nft_market', '_blank')}>Source Code</button>
+        <div className={styles.flexCenter}>
+          <img 
+            src={assets.scene}
+            alt='download img'
+            className={styles.fullImg}
+          />
+        </div>
+      </div>
+    </div>
   )
 }
 
